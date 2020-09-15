a51834ea255846af00933db573635def1a0fbd4c
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-09-15 18:24:36 +0500

    ...

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+// connectDB();
 // app.get("/uploads/:file", (req, res) => {
 //   res.sendFile(__dirname + "/client/public/uploads/" + req.params.file);
 // });
