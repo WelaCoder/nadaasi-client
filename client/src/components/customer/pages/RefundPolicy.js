08e3bfbff445602e564b7dbccf0ba112d582059a
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-09-25 22:56:30 +0500

    ...

--- client/src/components/customer/pages/RefundPolicy.js
+++ client/src/components/customer/pages/RefundPolicy.js
@@ -19,27 +19,27 @@ const RefundPolicy = () => {
         </header>
 
         <section style={{ marginTop: "50px", marginBottom: "100px", }} id="" class="page-content page-cms page-cms-8 my-font-futura">
-          <h1>Refund policy</h1>
-          <p>You have a 14-day right to return or exchange a product according to Finnish
+          <h1 className='my-font-futura'>Refund policy</h1>
+          <p className='my-font-futura'>You have a 14-day right to return or exchange a product according to Finnish
             consumer protection legislation. You have the right to return the order if:</p>
           <ol style={{ marginLeft: '50px' }}>
-            <li>The order has been cancelled according to the Terms &amp; Conditions by
+            <li className='my-font-futura'>The order has been cancelled according to the Terms &amp; Conditions by
               notifying NADAASI within 14 days of receiving the product, and</li>
-            <li>The product has not been used, and</li>
-            <li>The product is in the condition that you received it and all the parts (e.g.
+            <li className='my-font-futura'>The product has not been used, and</li>
+            <li className='my-font-futura'>The product is in the condition that you received it and all the parts (e.g.
               removable belts and hang tags) of the item(s) are included in the package, and</li>
-            <li>The item has been repackaged into its original package or an equivalent</li>
+            <li className='my-font-futura'>The item has been repackaged into its original package or an equivalent</li>
           </ol>
-          <p>The product must be returned immediately after cancellation. When returning
+          <p className='my-font-futura'>The product must be returned immediately after cancellation. When returning
           products, you will be responsible for potential damages and losses. The webshop
           returns are free of charge for customers in Finland. For customers abroad, we
             will refund return costs if the product we have sent you is incorrect or faulty.</p>
-          <p>After we have received the returned item(s) and all the criteria (1-4) listed
+          <p className='my-font-futura'>After we have received the returned item(s) and all the criteria (1-4) listed
           above are fulfilled, we will send you the new item or give you a refund. We will
           issue a refund following your method of payment. Refund times depend on the
           payment method you have selected. This can vary greatly between different
             payment methods.</p>
-          <p>Before sending any returns, please contact us by email: info@nadaasi.fi</p>
+          <p className='my-font-futura'>Before sending any returns, please contact us by email: info@nadaasi.fi</p>
         </section>
       </section>
     </div>
--- client/src/components/customer/pages/SecurePayment.js
+++ client/src/components/customer/pages/SecurePayment.js
@@ -22,27 +22,27 @@ const SecurePayment = () => {
             <section id="main">
 
               <header class="page-header">
-                <h1 class="tt-innerpagetitle">
+                <h1 class="tt-innerpagetitle my-font-futura">
                   Secure payment
                   </h1>
               </header>
 
-              <section id="#content" class="page-content page-cms page-cms-5">
-                <p>We use klarna as our online payment method that allows us to give customers a
+              <section id="#content" class="page-content page-cms page-cms-5 my-font-futura">
+                <p className='my-font-futura'>We use klarna as our online payment method that allows us to give customers a
                 personalised and smooth purchase experience. It includes several different
                 payment options, both Klarna’s own Payment Methods as well as such provided by
                     third parties.</p>
-                <p></p>
-                <p></p>
-                <h2>Pay Now</h2>
-                <p>Pay now allows customers to pay at the time of the purchase. The service
+                <p className='my-font-futura'></p>
+                <p className='my-font-futura'></p>
+                <h2 className='my-font-futura'>Pay Now</h2>
+                <p className='my-font-futura'>Pay now allows customers to pay at the time of the purchase. The service
                 enables customers to pay with a number of different payment methods, such as
                 direct debit, debit/credit card, direct banking and So forth. Pay now includes
                 both payment methods provided by Klarna itself as well as payment methods
                     provided by third parties.</p>
                 <p></p>
-                <h2>Pay Later</h2>
-                <p>Pay later offers interest-free deferred payments for customers who want to
+                <h2 className='my-font-futura'>Pay Later</h2>
+                <p className='my-font-futura'>Pay later offers interest-free deferred payments for customers who want to
                 buy with the minimum of fuss and then pay later at their convenience. There’s no
                 need for the consumer to provide lengthy personal details or card numbers to
                 make a purchase. Pay later gives customers full control and trust and can be
@@ -50,8 +50,8 @@ const SecurePayment = () => {
                 and deal with how to pay at a later stage, confident they won’t have to pay for
                     things they decide not to keep.</p>
                 <p></p>
-                <h2>Slice It</h2>
-                <p>Slice it is a payment method which offers consumers to finance their
+                <h2 className='my-font-futura'>Slice It</h2>
+                <p className='my-font-futura'>Slice it is a payment method which offers consumers to finance their
                 purchases over time. The type of credit offered is seen on the website. In cases
                 of account credit, consumers who do not yet have a Slice it account can apply
                 for one by placing a Slice it purchase. The response is always instant. Slice it
