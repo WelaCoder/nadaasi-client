28921e13029e5d0eaed5d5ff3f77210ca236718f
Zain Ul Abideen <zain.abideen14572@gmail.com>
2020-10-04 14:42:09 +0500

    ...

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,5 @@
 // pagination
-export const API = "https://nadassi-api.herokuapp.com";
+export const API = "https://nadaasi-api.uc.r.appspot.com";
+// export const API = "https://nadassi-api.herokuapp.com";
 // export const API = "http://localhost:3001";
 // TESTIMONIALS
